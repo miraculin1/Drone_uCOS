154a6f432bab07f5035622e7921be8eee5b2af3d
miraculin <2855193562@qq.com>
2023-05-09 20:40:29 +0800

    finish require for this term

--- Source/drivers/REC.c
+++ Source/drivers/REC.c
@@ -64,25 +64,3 @@ void initRec() {
   TIM1_CCER |= (0x1 << 0);
 }
 
-void TIM1_CC_IRQHandler() {
-  // reset CNT after each interrupt
-
-  // line up when possible
-  if (!recData.linedUp) {
-    if (TIM1_CCR1 >= 4000) {
-      recData.linedUp = 1;
-      LED_ON();
-    }
-  } else {
-    recData.chs[recData.now] = TIM1_CCR1;
-    if (recData.now == 8) {
-      recData.now = 0;
-      // check whether linedUp or not
-      if (recData.chs[8] < 3000) {
-        recData.linedUp = 0;
-      }
-    } else {
-      recData.now++;
-    }
-  }
-}
---
+++ Source/user_INT.c
@@ -0,0 +1,28 @@
+#include "Includes.h"
+
+void TIM1_CC_IRQHandler() {
+  OSIntEnter();
+  // reset CNT after each interrupt
+  TIM1->CNT = 0;
+
+  // line up when possible
+  if (!recData.linedUp) {
+    if (TIM1->CCR1 >= 4000) {
+      recData.linedUp = 1;
+      LED_ON();
+    }
+  } else {
+    recData.chs[recData.now] = TIM1->CCR1;
+    if (recData.now == 8) {
+      recData.now = 0;
+      // check whether linedUp or not
+      if (recData.chs[8] < 3000) {
+        recData.linedUp = 0;
+      }
+    } else {
+      recData.now++;
+    }
+  }
+
+  OSIntExit();
+}
